260279edde7193362246143544ce8e07f40abf89
actorh <60810552+actorh@users.noreply.github.com>
2020-05-26 22:02:04 +0800

    Create fenye.js

---
+++ fenye.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    new Vue({
+        el:'',
+        data:{
+            lists:[],
+        },
+    })
+    methods:{
+        getlist:function(){
+            Axios({
+                methods:'get',
+                url:''
+            })
+            .then(function(res){
+                console.log(res);
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+        }
+    }
+}
